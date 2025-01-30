ec09a2063e93d3815a7c5147e9f3cdd50da287ce
jaimearias <jaimeariaskankuy@gmail.com>
2025-01-30 09:39:58 -0500

    mi primer commit

---
+++ primero.py
@@ -0,0 +1,16 @@
+#variables y operadores aritmeticos.
+a = 10
+b = 3
+resultado1 = a + b
+resultado2 = a - b
+resultado3 = a * b
+resultado4 = a / b
+resultado5 = a % b
+resultado6 = a // b
+
+print("el resultado es", resultado1)
+print("el resultado es", resultado2)
+print("el resultado es", resultado3)
+print("el resultado es", resultado4)
+print("el resultado es", resultado5)
+print("el resultado es", resultado6)
