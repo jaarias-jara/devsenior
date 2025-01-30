3a4620bc3cbbabba4785eaacf0c82518077dac3c
jaimearias <jaimeariaskankuy@gmail.com>
2025-01-30 16:51:23 -0500

    agregue hola mundo a tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -4,3 +4,4 @@ print(frutas)
 #print(frutas2)
 print(frutas.count("naranja"))#sirve para contar cuantas naranjas tengo
 print(frutas.index("naranja"))#sirve para saber la posicion de la naranja
+print("hola mundo")
